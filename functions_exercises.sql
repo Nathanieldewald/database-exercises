5b5af6cfb66c3c0521fdb3ed08bcf692943016a2
Nathaniel Dewald <Dewald.nathaniel@gmail.com>
2023-03-10 13:41:14 -0600

    Added group_by_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,7 +1,7 @@
 SHOW DATABASES;
 USE employees;
 SHOW CREATE TABLE employees;
-
+SHOW CREATE TABLE salaries;
 /*
 2.Write a query to to find all employees whose last name starts and ends with 'E'. 
 Use concat() to combine their first and last name together as a single column named full_name.
@@ -35,4 +35,33 @@ Find all employees hired in the 90s and born on Christmas.
 Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
 */
 
+select *, datediff(curdate(), hire_date) as days
+from employees
+where hire_date like ('199%')
+and birth_date like ('%-12-25');
 
+/*
+Find the smallest and largest current salary from the salaries table.
+*/
+
+(select * 
+from salaries
+order by salary
+limit 1)
+union all
+(select *
+from salaries
+order by salary desc
+limit 1);
+
+/*
+Use your knowledge of built in SQL functions to generate a username for all of the employees. 
+A username should be all lowercase, and consist of the first character of the employees first name, 
+the first 4 characters of the employees last name, an underscore, the month the employee was born, 
+and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
+*/
+
+select lower(concat(substr(first_name,1,1), substr(last_name,1,4), '_', substr(birth_date,6,2), substr(birth_date, 3,2))) as username,
+first_name, last_name, birth_date
+from employees
+limit 10;
